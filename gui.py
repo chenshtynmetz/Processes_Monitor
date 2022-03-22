386d4e8a98046dad97455646ae630207af325e7b
chenshtynmetz <jiayhb@gmail.com>
2022-03-22 14:01:18 +0200

    commit 22/3 first gui

--- gui.py
+++ gui.py
@@ -1,9 +1,42 @@
 from tkinter import *
 from tkinter import ttk
+import tkinter as tk
+from monitor import Monitor
+from tkinter import messagebox
 
 class Gui:
     def __init__(self):
-        root = Tk()
-        frm = ttk.Frame(root, padding=10)
-        frm.grid()
-        root.mainloop()
+        self.root = Tk()
+        self.root.title("monitor")
+        self.root.geometry("850x850")
+        self.root.resizable(1, 1)
+        self.frm = ttk.Frame(self.root, padding=10)
+        self.frm.grid()
+        self.text = tk.StringVar()
+        ttk.Button(self.frm, text="monitor mode", command=self.monitor_mood).grid(column=1, row=0)
+        self.root.mainloop()
+
+    def monitor_mood(self):
+        time_root = Tk()
+        time_root.title("set time")
+        time_root.geometry("500x500")
+        time_frm = ttk.Frame(time_root, padding=10)
+        time_frm.pack(padx=10, pady=10, fill='x', expand=True)
+        # time_frm.grid()
+        l1 = ttk.Label(time_frm, text="please enter period of time in seconds:")
+        l1.pack(fill='x', expand=True)
+        # ttk.Entry(time_frm, widget=)
+        textbox = ttk.Entry(time_root, textvariable=self.text)
+        textbox.pack(fill='x', expand=True)
+        textbox.focus()
+        ok_button = ttk.Button(time_frm, text="OK", command=self.start_monitor)  #set command to exit
+        ok_button.pack(fill='x', expand=True, pady=5)
+        time_root.mainloop()
+
+    def start_monitor(self):
+        my_time = self.text.get()
+        Monitor(float(my_time))
+
+
+if __name__ == '__main__':
+    Gui()
